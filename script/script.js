1455582e6d02f9ca02dd8bc2e3308cd00319cc5a
t0lIk1 <118015233+t0lIk1@users.noreply.github.com>
2023-10-02 09:16:41 +0300

    icon update

--- script/script.js
+++ script/script.js
@@ -54,8 +54,8 @@ let store = {
       <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-облако-64.png"></img>${store.clouds}%</li>
       <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-влажность-64.png"></img>${store.humidity}%</li>
       <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-барометр-64.png"></img>${store.pressure} hPa</li>
-      <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-восход-64.png"></img>${showSunUpTimes(store.sunrise)}</li>
-      <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-закат-солнца-64.png"></img>${showSunDownTimes(store.sunset)}</li>
+      <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-восход-64.png"></img>${transtateTime(store.sunrise)}</li>
+      <li class="hero__item"><img class="option-ico" src="./img/option icon/icons8-закат-солнца-64.png"></img>${transtateTime(store.sunset)}</li>
       </ul>
     </div>
   </div>`;
@@ -65,89 +65,47 @@ let store = {
     const value = main.toLowerCase();
     switch(value){
       case 'clouds':
-        return 'icons8-partly-cloudy-day-100.png';
-      case 'rain':
-        return 'icons8-rain-100.png';
-      case 'snow':
-        return 'icons8-snow-100.png';
-      case 'storm':
-        return 'icons8-storm-100.png';
-      case 'fog':
-        return 'icons8-fog-100.png';
-      case 'sleet':
-        return 'icons8-sleet-day-100.png';
-      case 'clear':
-        return 'icons8-sun-100.png';
-      default:
-        return 'the.png';
-    }
+      return 'overcast.png';
+    case 'rain':
+      return 'rain.png';
+    case 'drizzle':
+      return 'rain.png';
+    case 'snow':
+      return 'snow.png';
+    case 'thunderstorm':
+      return 'thunder.png';
+    case 'storm':
+      return 'rain_thunder.png';
+    case 'fog':
+      return 'fog.png';
+    case 'sleet':
+      return 'sleet.png';
+    case 'clear':
+      return 'day_clear.png';
+    default:
+      return 'the.png';
+  }
   };
-  function showSunUpTimes(sunriseUnix) {
-    const datesunUP = new Date(sunriseUnix*1000);
-    let hours = datesunUP.getUTCHours();
+function transtateTime(Time) {
+    const date = new Date(Time*1000);
+    let hours = date.getUTCHours();
     if(hours < 10){
       hours = '0' + hours;
     }
-    let minutes = datesunUP.getUTCMinutes();
+    let minutes = date.getUTCMinutes();
     if(minutes < 10){
       minutes = '0' + minutes;
     }
-    let seconds = datesunUP.getUTCSeconds();
-    if(seconds < 10){
-      seconds = '0' + seconds;
-    }
-    return `${hours}:${minutes}:${seconds}`;
+    return `${hours}:${minutes}`;
 }
-function showSunDownTimes(sunsetUnix){
-  const datesunDown = new Date(sunsetUnix*1000);
-  let hours = datesunDown.getUTCHours();
-  if(hours < 10){
-    hours = '0' + hours;
-  }
-  let minutes = datesunDown.getUTCMinutes();
-  if(minutes < 10){
-    minutes = '0' + minutes;
-  }
-  let seconds = datesunDown.getUTCSeconds();
-  if(seconds < 10){
-    seconds = '0' + seconds;
-  }
-  return `${hours}:${minutes}:${seconds}`;
-}
-function tratslateDeg(windDeg){
-  if(windDeg >= 0 || windDeg === 360){
-    if(windDeg === 0 || windDeg === 360){
-      return 'N';
-    }
-    else if(windDeg > 0 && windDeg < 90){
-      return 'NE';
-    }
-  }
-  if(windDeg >= 90){
-    if (windDeg === 90){
-      return 'E';
-    }
-    else if(windDeg >= 90 && windDeg < 180){
-      return 'SE';
-    }
 
-  }
-  if(windDeg >= 180){
-    if(windDeg === 180){
-      return 'S';
+function tratslateDeg(windDeg){
+  let direction = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW'];
+    if (windDeg === 360 ){
+      
     }
-    else if(windDeg > 180 && windDeg < 270){
-      return 'SW';
-    }
-  }
-  if(windDeg >= 270){
-    if(windDeg === 270){
-      return 'W';
-    }
-    else if(windDeg > 270 && windDeg < 360){
-      return 'NW';
-    }
-  }
+  significance = Math.round(windDeg / 45);
+  return direction[significance];
 }
 
   const elemenEdit = () => {
